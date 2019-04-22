67002f2e5c7986625a505abe85f974baa0d40e3e
Cliff <cliff.crerar@gmail.com>
2019-04-23 00:31:57 +0200

    First commit of new excercise

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RESTClient_sql_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
